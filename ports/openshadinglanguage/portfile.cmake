b8b8e642870418791bac581e5dcb553cda33fa50
romainaugier <augier.romain@sfr.fr>
2025-03-11 17:51:53 +0100

    More work on windows build...

--- ports/openshadinglanguage/portfile.cmake
+++ ports/openshadinglanguage/portfile.cmake
@@ -22,12 +22,31 @@ if(VCPKG_TARGET_IS_WINDOWS)
 
     set(BISON_ROOT "${FLEX_BISON_SOURCE_PATH}/bin/Release")
     message(STATUS "BISON_ROOT=${BISON_ROOT}")
+
+    # set(LLVM_VERSION 18.1.8)
+
+    # get_filename_component(PARENT_DIR ${FLEX_BISON_SOURCE_PATH} DIRECTORY)
+
+    # set(LLVM_INSTALL_DIR "${PARENT_DIR}/llvm")
+    # set(LLVM_FILE_NAME "clang+llvm-${LLVM_VERSION}-x86_64-pc-windows-msvc")
+
+    # vcpkg_download_distfile(
+    #     LLVM_INSTALL_FILE
+    #     URLS "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VERSION}/${LLVM_FILE_NAME}.tar.xz"
+    #     FILENAME "${LLVM_FILE_NAME}.tar.xz"
+    #     SHA512 77a1028963f46930ff2208f69614f0eea3f57ad3fb536b198337ec38ada19d2f443eabaa99852787524d227deb2d513ceee5cf468543f2916ad2578deafc7158
+    # )
+
+    # if(NOT EXISTS ${LLVM_INSTALL_DIR})
+    #     vcpkg_extract_archive(
+    #         ARCHIVE ${LLVM_INSTALL_FILE}
+    #         DESTINATION ${LLVM_INSTALL_DIR}
+    #     )
+    # endif()
 elseif(VCPKG_TARGET_IS_LINUX)
     vcpkg_find_acquire_program(FLEX)
     vcpkg_find_acquire_program(BISON)
 endif()
-
-vcpkg_find_acquire_program(CLANG)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -41,6 +60,7 @@ vcpkg_cmake_configure(
         -DUSE_QT6=OFF
         -DBISON_ROOT="${BISON_ROOT}"
         -DFLEX_ROOT="${BISON_ROOT}"
+        # -DLLVM_ROOT="${LLVM_INSTALL_DIR}/${LLVM_FILE_NAME}"
     )
 
 vcpkg_cmake_install(DISABLE_PARALLEL)
